7b6c111eb22c8b746d3cb428905e40b347dba457
dhruvinparikh <imsilent93@gmail.com>
2023-12-31 01:30:54 +0000

    (ref):  wasmer replace with wastime

--- wasmtime/main.go
+++ wasmtime/main.go
@@ -3,10 +3,10 @@ package main
 import (
 	"fmt"
 
-	"github.com/wasmerio/wasmer-go/wasmer"
+	"github.com/bytecodealliance/wasmtime-go/v12"
 )
 
-// https://pkg.go.dev/github.com/wasmerio/wasmer-go@v1.0.4/wasmer#hdr-Examples
+// https://pkg.go.dev/github.com/bytecodealliance/wasmtime-go#example-Config-Fuel
 func main() {
 	// Let's assume we don't have WebAssembly bytes at hand. We
 	// will write WebAssembly manually.
@@ -20,15 +20,18 @@ func main() {
 	  (export "sum" (func 0)))
 `)
 
-	// Create an Engine
-	engine := wasmer.NewEngine()
+    cfg := wasmtime.NewConfig()
+    cfg.SetConsumeFuel(true)
+	cfg.CacheConfigLoadDefault()
+	cfg.SetStrategy(wasmtime.StrategyCranelift)
+	store := wasmtime.NewStore(wasmtime.NewEngineWithConfig(cfg))
 
-	// Create a Store
-	store := wasmer.NewStore(engine)
+	err := store.AddFuel(10000000000000) // testing only ;)
+	if err != nil {
+		fmt.Println("Failed to add fuel:", err)
+	}
 
-	// Let's compile the module.
-	module, err := wasmer.NewModule(store, wasmBytes)
-
+	module, err := wasmtime.NewModule(store.Engine, wasmBytes)
 	if err != nil {
 		fmt.Println("Failed to compile module:", err)
 	}
@@ -43,18 +46,18 @@ func main() {
 		panic(fmt.Sprintln("Failed to instantiate the module:", err))
 	}
 
-	// Now let's execute the `sum` function.
-	sum, err := instance.Exports.GetFunction("sum")
+	// // Now let's execute the `sum` function.
+	// sum, err := instance.Exports.GetFunction("sum")
 
-	if err != nil {
-		panic(fmt.Sprintln("Failed to get the `add_one` function:", err))
-	}
+	// if err != nil {
+	// 	panic(fmt.Sprintln("Failed to get the `add_one` function:", err))
+	// }
 
-	result, err := sum(1, 2)
+	// result, err := sum(1, 2)
 
-	if err != nil {
-		panic(fmt.Sprintln("Failed to call the `add_one` function:", err))
-	}
+	// if err != nil {
+	// 	panic(fmt.Sprintln("Failed to call the `add_one` function:", err))
+	// }
 
-	fmt.Println("Results of `sum`:", result)
+	// fmt.Println("Results of `sum`:", result)
 }
