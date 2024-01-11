7162769f8e812637db34c8521e65c4eb065b94c9
dhruvinparikh <imsilent93@gmail.com>
2024-01-11 14:59:54 +0000

    (fix): SIGKILL error

--- wasmtime-sample/c.go
+++ wasmtime-sample/c.go
@@ -1,44 +1,9 @@
 package main
 
 /*
-#include <gnu/libc-version.h>
-#include <pthread.h>  // pthread_atfork might be required
-
-// Other stub functions
-int __longjmp_chk() { return 0; }
+#cgo LDFLAGS: -static-libgcc
+int gnu_get_libc_version() { return 0; }
+int __register_atfork() { return 0; }
 int __res_init() { return -1; }
-int __sigsetjmp() { return 0; }
-
-// Declare _register_atfork if not available
-#ifndef _GNU_SOURCE
-int __register_atfork(void (*prepare)(void), void (*parent)(void), void (*child)(void), void* dso_handle) {
-    return pthread_atfork(prepare, parent, child);
-}
-#endif
-// Declare gnu_get_libc_version if not available
-#ifndef _GNU_SOURCE
-const char *gnu_get_libc_version(void) {
-    // Attempt to retrieve version information using preprocessor directives
-    #if defined(__GLIBC__)
-        #if defined(__GLIBC_PREREQ)
-            // GNU C Library version information using __GLIBC_PREREQ macro
-            #if __GLIBC_PREREQ(2, 30)
-                return "GNU C Library version 2.30 or later";
-            #elif __GLIBC_PREREQ(2, 29)
-                return "GNU C Library version 2.29";
-            // Add more version checks as needed
-            #else
-                return "Unknown GNU C Library version";
-            #endif
-        #else
-            // Fallback for systems without __GLIBC_PREREQ (older versions)
-            return "Unknown GNU C Library version (No __GLIBC_PREREQ)";
-        #endif
-    #else
-        // Not using GNU C Library
-        return "Not using GNU C Library";
-    #endif
-}
-#endif
 */
 import "C"
